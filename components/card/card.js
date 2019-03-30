22435d5adbc09403ae5aa416d4093819bbc92ae6
FanMLei <15638515832@163.com>
2019-03-30 23:53:41 +0800

    增加数字动态效果

--- components/card/card.js
+++ components/card/card.js
@@ -1,8 +1,5 @@
-// components/card.js
+import CountUp from '../../utils/count-up.js'
 Component({
-  /**
-   * 组件的属性列表
-   */
   properties: {
     bgColor:{
       type:String,
@@ -30,17 +27,8 @@ Component({
    * 组件的初始数据
    */
   data: {
-    
   },
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-    
-  },
-  lifetimes: {
-    ready(){
 
-    }
-  }
+  methods: {
+  },
 })
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,8 +1,8 @@
 const {
   $Toast
 } = require('../../dist/base/index');
-
 import api from '../../api/api.js'
+import CountUp from '../../utils/count-up.js'
 var wxCharts = require('../../utils/wxcharts-min.js');
 var lineChart = null
 Page({
@@ -24,8 +24,7 @@ Page({
       "num": 0,
       "name": 'data_num'
     }],
-    increaseInfo: [
-      {
+    increaseInfo: [{
         "title": "当前在线设备",
         "num": 0,
         "name": "onlineDeviceNum"
@@ -56,6 +55,19 @@ Page({
         this.setData({
           totalInfo: res.data.data
         })
+        // 增加数字显示动画效果
+        new CountUp(0, this.data.totalInfo[0].num, 0, 1, count => this.setData({
+          ['totalInfo[0].num']: count
+        })).start();
+        new CountUp(0, this.data.totalInfo[1].num, 0, 1, count => this.setData({
+          ['totalInfo[1].num']: count   
+        })).start();
+        new CountUp(0, this.data.totalInfo[2].num, 0, 1, count => this.setData({
+          ['totalInfo[2].num']: count   
+        })).start();
+        new CountUp(0, this.data.totalInfo[3].num, 0, 1, count => this.setData({
+          ['totalInfo[3].num']: count   
+        })).start();
       }
     })
     api.increaseInfo((res) => {
@@ -64,11 +76,20 @@ Page({
         this.setData({
           increaseInfo: res.data.data
         })
+        new CountUp(0, this.data.increaseInfo[0].num, 0, 1, count => this.setData({
+          ['increaseInfo[0].num']: count
+        })).start();
+        new CountUp(0, this.data.increaseInfo[1].num, 0, 1, count => this.setData({
+          ['increaseInfo[1].num']: count
+        })).start();
+        new CountUp(0, this.data.increaseInfo[2].num, 0, 1, count => this.setData({
+          ['increaseInfo[2].num']: count
+        })).start();
       }
     })
     api.trendInfo((res) => {
       $Toast.hide()
-      if(res.data.code==0){
+      if (res.data.code == 0) {
         var x = []
         var y = []
         for (var i = 0; i < res.data.data.length; i++) {
@@ -77,20 +98,20 @@ Page({
         }
         lineChart = new wxCharts({
           canvasId: 'trend', //canvasID
-          type: 'line',   //类型
+          type: 'line', //类型
           categories: x,
-          legend:false,     //不显示下面的标识
-          animation: true,  //启用动画效果
+          legend: false, //不显示下面的标识
+          animation: true, //启用动画效果
           // background:'#ffffff',  //背景颜色
           series: [{
             name: '',
             data: y,
-            format: function (val, name) {
+            format: function(val, name) {
               return val;
             }
           }],
           xAxis: {
-            disableGrid: true  
+            disableGrid: true
           },
           width: windowWidth,
           height: 200,
@@ -103,9 +124,9 @@ Page({
       }
     })
   },
-  touchHandler: function (e) {
+  touchHandler: function(e) {
     lineChart.showToolTip(e, {
-      format: function (item, category) {
+      format: function(item, category) {
         return category + ':' + item.data
       }
     });
@@ -115,13 +136,13 @@ Page({
     api[event.currentTarget.dataset.name]((res) => {
       console.log(res.data)
       if (res.data.code === 0) {
-        this.setData({
-          ['increaseInfo[' + event.currentTarget.dataset.index + '].num']: res.data.data
-        })
+        // 动画刷新数据
+        new CountUp(0, res.data.data, 0, 1, count => this.setData({
+          ['increaseInfo[' + event.currentTarget.dataset.index + '].num']: count
+        })).start();
       }
     })
   },
- 
   //下拉刷新
   onPullDownRefresh() {
     wx.stopPullDownRefresh()
