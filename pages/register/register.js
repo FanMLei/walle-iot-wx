47d9be1b75e892a29330fd888cb456014e7bf4b5
FanMLei <15638515832@163.com>
2019-04-20 22:47:39 +0800

    更改注册页面样式和逻辑

--- pages/register/register.js
+++ pages/register/register.js
@@ -2,77 +2,115 @@
 import api from '../../api/api.js'
 Page({
   data: {
-    error_info:'',
+    error_info: '',
+    // 提交按钮状态
     disabled: true,
+    // 输入框错误显示
+    uflag: true,
+    p1flag: true,
+    p2flag: true,
+    eflag: false,
+    // form表单
     username: '',
-    password: '',
     password1: '',
+    password2: '',
     email: '',
   },
-  check: function () {
-    var reg = /^\w+((.\w+)|(-\w+))@[A-Za-z0-9]+((.|-)[A-Za-z0-9]+).[A-Za-z0-9]+$/;
+  check: function() {
+    if (!this.data.uflag && !this.data.p1flag && !this.data.p2flag && !this.data.eflag){
+      this.setData({
+        disabled:false
+      })
+    }
+    else{
+      this.setData({
+        disabled:true
+      })
+    }
+  },
+  register(e) {
+    api.register(
+      this.data.username,
+      this.data.password1,
+      this.data.email
+    )
+  },
+
+  setUsername(e) {
+    this.setData({
+      username: e.detail.detail.value
+    })
     if (this.data.username.length < 4) {
       this.setData({
         error_info: '登录名不合要求，至少4个字符',
-        disabled: true
+        uflag: true
       })
-    } else if (this.data.password.length < 8) {
-      this.setData({
-        error_info: '密码长度过短，请重新输入！',
-        disabled: true
-      })
-    }
-    else if (this.data.password != this.data.password1) {
-      this.setData({
-        error_info: '确认密码错误！',
-        disabled: true
-      })
-    }else if(!reg.test(this.data.email)){
-      this.setData({
-        error_info: '邮箱格式错误！',
-        disabled: true
-      })
-    }else {
+    } else {
       this.setData({
         error_info: '',
-        disabled: false
+        uflag: false
       })
     }
-  },
-  onLoad: function (options) {
-
-  },
-  formSubmit(e) {
-    api.register(
-      e.detail.value.username, 
-      e.detail.value.password, 
-      e.detail.value.email
-    )
-  },
- 
-  setUsername(e) {
-    this.setData({
-      username: e.detail.value
-    })
-    this.check()
-  },
-  setPassword(e) {
-    this.setData({
-      password: e.detail.value
-    })
     this.check()
   },
   setPassword1(e) {
     this.setData({
-      password1: e.detail.value
+      password1: e.detail.detail.value
     })
+    if (this.data.password1.length < 8) {
+      this.setData({
+        error_info: '密码长度过短，请重新输入！',
+        p1flag: true,
+      })
+    } else {
+      this.setData({
+        error_info: '',
+        p1flag: false,
+      })
+    }
+    this.check()
+  },
+  setPassword2(e) {
+    this.setData({
+      password2: e.detail.detail.value
+    })
+    if (this.data.password2 != this.data.password1) {
+      this.setData({
+        error_info: '确认密码错误！',
+        p2flag: true,
+      })
+    } else {
+      this.setData({
+        error_info: '',
+        p2flag: false,
+      })
+    }
     this.check()
   },
   setEmail(e) {
     this.setData({
-      email: e.detail.value
+      email: e.detail.detail.value
     })
+    var reg = /^\w+((.\w+)|(-\w+))@[A-Za-z0-9]+((.|-)[A-Za-z0-9]+).[A-Za-z0-9]+$/;
+    if (this.data.email){
+      if (!reg.test(this.data.email)) {
+        this.setData({
+          error_info: '邮箱格式错误！',
+          eflag: true
+        })
+      } else {
+        this.setData({
+          error_info: '',
+          eflag: false
+        })
+      }
+    }
+    else{
+      this.setData({
+        error_info: '',
+        eflag: false
+      })
+    }
     this.check()
   },
-  
 })
