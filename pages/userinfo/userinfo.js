fd685f2edd840b73b13d27065e2ac9e1bce450a4
FanMLei <15638515832@163.com>
2019-04-21 20:13:57 +0800

    完善个人信息页

--- pages/userinfo/userinfo.js
+++ pages/userinfo/userinfo.js
@@ -1,66 +1,28 @@
-// pages/userinfo/userinfo.js
+import api from '../../api/api.js'
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    edit:true,
+    username:'',
+    uid:'',
+    email:'',
+    sex:'',
+    birthday:'',
+    tel:'',
+    address:'',
+    introduction:''
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    api.userInfo((res)=>{
+     console.log(res.data)
+     this.setData(
+       res.data.data
+     )
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  edit(){
+    this.setData({
+      edit:false
+    })
   }
 })
